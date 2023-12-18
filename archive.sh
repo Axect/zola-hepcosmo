4570b0705eae71d41773a8ac383b40a01ce17f12
axect <ax2ct@outlook.com>
2023-12-18 12:42:03 +0900

    Archive Test

---
+++ archive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Current date in YYMMDD format
+current_date=$(date +%y%m%d)
+
+# Directory containing the files
+content_dir="content"
+archive_dir="content_archived"
+
+# Create the archive directory if it doesn't exist
+mkdir -p "$archive_dir"
+
+# Loop through all .md files in the content directory
+for file in "$content_dir"/??????_*.md; do
+    # Extract the date from the filename
+    filename=$(basename "$file")
+    file_date=${filename:0:6}
+
+    # Check if the file date is less than the current date
+    if [[ "$file_date" < "$current_date" ]]; then
+        # Move the file to the archived directory
+        mv "$file" "$archive_dir/"
+    fi
+done
--- content/first.md
+++
@@ -1,9 +0,0 @@
-+++
-title = "Shinchon GW Meeting"
-date = 2024-01-09 10:30:00
-
-[taxonomies]
-categories = ["Meeting"]
-+++
-
-Hello
